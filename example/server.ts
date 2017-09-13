c942175929f178bf15797bf1f0351b984b4bfe65
Andrew Throener <athroener@mystartupsolutions.com>
2017-09-13 14:05:52 -0500

    Quick Release Patch

--- example/server.ts
+++ example/server.ts
@@ -1,22 +1,33 @@
 import * as glue from "glue";
 import * as hapi from "hapi";
 import { IInvoicingOptions} from "../";
-import GlueManifest from "../src/glue";
+import {
+    hapiIntacctGlueRegistration,
+    hapiPayPalGlueRegistration,
+    hapiPayPalIntacctInvoicingGlueRegistration,
+} from "../src/glue";
 
 if (!module.parent) {
-    (GlueManifest as any).connections = [
-        {
-            host: process.env.IP || "127.0.0.1",
-            labels: ["private"],
-            port: process.env.PRIVATE_PORT || 3001,
-        },
-        {
-            host: process.env.IP || "0.0.0.0",
-            labels: ["public"],
-            port: process.env.PUBLIC_PORT || process.env.PORT || 3000,
-        },
-    ];
-    glue.compose(GlueManifest).then(async (server: hapi.Server) => {
+    const manifest = {
+        connections: [
+            {
+                host: process.env.IP || "127.0.0.1",
+                labels: ["private"],
+                port: process.env.PRIVATE_PORT || 3001,
+            },
+            {
+                host: process.env.IP || "0.0.0.0",
+                labels: ["public"],
+                port: process.env.PUBLIC_PORT || process.env.PORT || 3000,
+            },
+        ],
+        registrations: [
+            hapiIntacctGlueRegistration,
+            hapiPayPalGlueRegistration,
+            hapiPayPalIntacctInvoicingGlueRegistration,
+        ],
+    };
+    glue.compose(manifest).then(async (server: hapi.Server) => {
         await server.start();
         // tslint:disable-next-line:no-console
         console.log("Started!");
--- src/glue/index.ts
+++ src/glue/index.ts
@@ -1,16 +1,3 @@
-import { PluginRegistrationObject } from "hapi";
-import { hapiIntacctGlueRegistration } from "./hapi-intacct";
-import { hapiPayPalGlueRegistration } from "./hapi-paypal";
-import { hapiPayPalIntacctInvoicingGlueRegistration } from "./invoicing";
-
 export { hapiIntacctGlueRegistration } from "./hapi-intacct";
 export { hapiPayPalGlueRegistration } from "./hapi-paypal";
 export { hapiPayPalIntacctInvoicingGlueRegistration } from "./invoicing";
-
-export default {
-    registrations: [
-        hapiIntacctGlueRegistration,
-        hapiPayPalGlueRegistration,
-        hapiPayPalIntacctInvoicingGlueRegistration,
-    ],
-};
